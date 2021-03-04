6fe59f45fff1de59fd9b8927dc43fbc85cea8f9d
Saumya1010 <63896909+Saumya1010@users.noreply.github.com>
2021-03-04 17:36:44 +0530

    Update mail.js

--- services/mail.js
+++ services/mail.js
@@ -1,6 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(
-  "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"
+//   "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"
 );
 
 const sendMail = ({
